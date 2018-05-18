bf7d2c7c6daa9bc392ebd660047d6cadfbdc85db
Ryan King <hello@ryanking.com>
2018-05-18 01:56:49 -0400

    Generalize function for adding key bindings and update gitignore

--- reddix.go
+++ reddix.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"log"
+	"unicode"
 
 	"github.com/jroimartin/gocui"
 	// "github.com/jzelinskie/geddit"
 )
+
+// setKey sets a key (upercase or lowercase) with a specified function that
+// applies to any view
+func setKey(g *gocui.Gui, key rune, handler func(*gocui.Gui, *gocui.View) error) {
+	lowcase := unicode.ToLower(key)
+	upcase := unicode.ToUpper(key)
+	if err := g.SetKeybinding("", lowcase, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
+	if err := g.SetKeybinding("", upcase, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
+}
 
 func main() {
 	// Initialize GUI
@@ -18,8 +32,8 @@ func main() {
 	// Layout & Keybindings
 	g.SetManagerFunc(frontpage)
 
-	err = g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit)
-	if err != nil {
+	setKey(g, 'q', quit)
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
 		log.Panicln(err)
 	}
 
